282158beb393959337437d62b751f0fd684a9fe4
martint-dev <martin.t5688@gmail.com>
2020-08-19 15:11:05 +0300

    Final product

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 const Input = document.querySelector('.todo-input');
 const Button = document.querySelector('.input-bttn');
 const List = document.querySelector('.items-list');
+const Filter = document.querySelector('.filter-todo-items');
 
 //Listener
+document.addEventListener('DOMContentLoaded', getItems);
 Button.addEventListener('click', addItem);
 List.addEventListener('click', deleteCheck);
 List.addEventListener('click', checkBttnCheck);
+Filter.addEventListener('click', filterList);
+
 
 //Functions
 
@@ -24,6 +28,10 @@ function addItem(event){
     newItem.innerText = Input.value;
     newItem.classList.add('todo-item');
     itemDiv.appendChild(newItem);
+
+    //Add item to local storage
+    saveLocalItems(Input.value);
+
 
     //Checked bttn
     const checkBttn = document.createElement('button');
@@ -55,6 +63,7 @@ function deleteCheck(event){
 
         //Add delete transition
         deleteParent.classList.add('fall');
+        removeLocalItems(deleteParent);
         
         //Delete the parent
         deleteParent.addEventListener('transitionend', function(){
@@ -79,3 +88,108 @@ function checkBttnCheck(event){
         checkParent.classList.toggle('completed');
     }
 }
+
+
+//Filter the list
+function filterList(e){
+    const items = List.childNodes;
+
+    items.forEach(function(item){
+        switch(e.target.value){
+            case "all":
+                item.style.display = 'flex';
+                break;
+            case "checked":
+                if(item.classList.contains('completed')){
+                    item.style.display = 'flex';
+                }else {
+                    item.style.display = 'none';
+                }
+                break;
+            case "unchecked":
+                if(!item.classList.contains('completed')){
+                    item.style.display = 'flex';
+                }else {
+                    item.style.display = 'none';
+                }
+                break;
+            
+        }
+    })
+}
+
+
+//Save items in local storage
+function saveLocalItems(item){
+    //Check the local storage
+    let items;
+
+    if(localStorage.getItem('items')=== null){
+        items = [];
+    }else{
+        items = JSON.parse(localStorage.getItem('items'));
+
+    }
+
+    items.push(item);
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+//Get items from local storage
+function getItems(){
+
+     //Check the local storage
+     let items;
+
+     if(localStorage.getItem('items')=== null){
+         items = [];
+     }else{
+         items = JSON.parse(localStorage.getItem('items'));
+ 
+     }
+
+    items.forEach(function(item){
+         //Item div
+    const itemDiv = document.createElement('div');
+    itemDiv.classList.add('item');
+
+    //LI
+    const newItem = document.createElement('li');
+    newItem.innerText = item;
+    newItem.classList.add('todo-item');
+    itemDiv.appendChild(newItem);
+
+    //Checked bttn
+    const checkBttn = document.createElement('button');
+    checkBttn.innerHTML = '<i class="fas fa-check"></i>';
+    checkBttn.classList.add('check-bttn');
+    itemDiv.appendChild(checkBttn);
+
+    //Delete bttn
+    const deleteBttn = document.createElement('button');
+    deleteBttn.innerHTML= '<i class="fas fa-trash"></i>';
+    deleteBttn.classList.add('delete-bttn');
+    itemDiv.appendChild(deleteBttn);
+
+    //Append all items in the list and clear the input value
+    List.appendChild(itemDiv);
+     })
+}
+
+//Remove item from local storage
+function removeLocalItems(item){
+      //Check the local storage
+      let items;
+
+      if(localStorage.getItem('items')=== null){
+          items = [];
+      }else{
+          items = JSON.parse(localStorage.getItem('items'));
+      }
+        //Finding the index of the item that we want to remove
+      const itemIndex = item.children[0].innerText;
+        //Removing it
+      items.splice(items.indexOf(itemIndex) , 1);
+      localStorage.setItem('items',JSON.stringify(items));
+}
+
